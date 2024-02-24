dcfbc9dc64d22f50bd7e04cee25ea47731462727
gabrielebottari <gabriele.bottari1@gmail.com>
2024-02-24 16:38:29 +0100

    ADD bonus page style and favicon

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -5,8 +5,11 @@
 @endsection
 
 @section("main-content")
-<div class="container text-center p-5">
-    <h1>Shop</h1>
-    <p class="text-center p-3">Pagina in fase di allestimento</p>
+<div class="bg-warning-subtle">
+    <div class="container text-center p-5">
+        <h1 class="fw-bold fs-1 pb-3">Shop</h1>
+        <p class="text-danger fs-1 fw-bold border border-danger border-5 rounded-5 text-uppercase">This page is under construction, please be patient <i class="fa-solid fa-hand"></i></p>
+        <img src="{{ Vite::asset('resources/img/wip.webp') }}" alt="wip" class="w-50">
+    </div>
 </div>
 @endsection
